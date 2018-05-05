14ff4d126a469e05962575325343341d61f2e1f5
= <=>
2018-05-05 09:09:32 -0400

    loop update

--- loop_spark.py
+++ loop_spark.py
@@ -18,39 +18,30 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import format_string
 from pyspark.sql.functions import date_format
+import scipy.stats as stats
+from scipy.spatial.distance import pdist, squareform
 
-
-# calculate Euclidean distance
-def distance(vector1, vector2):
-    distance = np.sum((vector1 - vector2) ** 2)
-    return distance
 
 # iteration used for each data point to get its
 # 1. probablilistic set distance
 # 2. its k-nearest neighbors
-def loop_single_object(data, single, k, extent):
-    all_dis = []
-    for i in range(len(data)):
-        dis = distance(data[i], single)
-        all_dis.append(dis)
-    all_dis = np.array(all_dis)
+def loop_single_object(data, i, k, extent):
+    
+    all_dis = data[i]
     all_dis_index_sorted = np.argsort(all_dis)
-    k_neighbor_points = data[all_dis_index_sorted][1:k+1]
+    k_neighbor_points_index = all_dis_index_sorted[1:k+1]
     k_neighbor_sum = np.sum(all_dis[all_dis_index_sorted][1:k+1])
-
+    
     #standard distance
     stand_diff = np.sqrt(k_neighbor_sum / k)
     #probablilistic set distance
     prob_set_diff = extent * stand_diff
-    return prob_set_diff, k_neighbor_points
+    return prob_set_diff, k_neighbor_points_index
 
 # used to calculate the probablilistic set distance for a single data point s in the context set of o (s belongs to S(o))
-def loop_single_S(data, single, k, extent):
-    all_dis = []
-    for i in range(len(data)):
-        dis = distance(data[i], single)
-        all_dis.append(dis)
-    all_dis = np.array(all_dis)
+def loop_single_S(data, i, k, extent):
+    
+    all_dis = data[i]
     all_dis_sorted = np.sort(all_dis)
     k_neighbor_sum = np.sum(all_dis_sorted[1:k+1])
     stand_diff = np.sqrt(k_neighbor_sum / k)
@@ -67,23 +58,27 @@ def ev_sd(data, k_neibor_point, k, extent):
     mean = np.mean(all_dis)
     return mean
 
-# loop
-def loop(data, k = 20, extent = 3):
+# get the loop square for each data point
+def loop(data, k = 20, extent = 2):
     result = []
     PLOF_list = []
     for i in range(len(data)):
-        prob_set_diff, k_neighbor_points = loop_single_object(data, data[i],k ,extent)
-        expected_pst = ev_sd(data, k_neighbor_points, k,extent)
-        PLOF = (prob_set_diff / expected_pst) - 1
-        PLOF_list.append(PLOF)
+        prob_set_diff, k_neighbor_points_index = loop_single_object(data, i,k ,extent)
+        expected_pst = ev_sd(data, k_neighbor_points_index, k,extent)
+        # if the expected value of probablilistic set distance for all objects in the context set of a data point is 0, this data point would have a PLOF value of 0
+        if (expected_pst == 0):
+            PLOF = 0
+            PLOF_list.append(PLOF)
+        else:
+            PLOF = (prob_set_diff / expected_pst) - 1.0
+            PLOF_list.append(PLOF)
     PLOF_list = np.array(PLOF_list)
-
-    # nPLOF is the square root of the expected value of squared PLOF values in the PLOF_list
+    # nPLOF is the square root of the expected value of all squared PLOF values among all data points
     nPLOF = extent * np.sqrt(np.mean(np.square(PLOF_list)))
 
     # get loop score
     for j in range(len(data)):
-        res = np.maximum(0, erf(PLOF_list[j] / (nPLOF * np.sqrt(2))))
+        res = np.maximum(0, erf(PLOF_list[j] / (nPLOF * np.sqrt(2.0))))
         result.append(res)
     return np.array(result)
 
@@ -98,10 +93,12 @@ pd_df = data.toPandas()
 pd_df = pd_df.select_dtypes(include=['float64','int64'])
 
 nonna = pd_df.dropna()
-data_val = nonna.values
 
-# loop with k set to 10
-loop_score = loop(data_val, k=10)
+# get pairwise distance squared among all the data points
+data_val = squareform(pdist(nonna))**2
+
+# loop with k set to 20
+loop_score = loop(data_val, k=20)
 
 nonna['loop'] = loop_score
 
